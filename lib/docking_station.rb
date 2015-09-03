24863143d3365b2847aec8089093ff0c4918f9d2
Jongmin Kim <kjm141215@gmail.com>
2015-09-03 16:44:13 +0100

    Finished Post Challenge1

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,10 +2,11 @@ require_relative 'bike'
 
 class DockingStation
   def release_bike
-    Bike.new
+    raise 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
-  	
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,11 +4,18 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it "releases working bikes" do
-  bike = subject.release_bike
-  expect(bike).to be_working
+    subject.dock(Bike.new)
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
-  it { is_expected.to respond_to :dock }
-  it 'should accept Bike.new as a argument' do 
-  	expect{ subject.dock(Bike.new) }.to_not raise_error
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  # it { is_expected.to respond_to(:dock) }
+  # it 'should accept Bike.new as an argument' do
+  # 	expect{ subject.dock(Bike.new) }.to_not raise_error
+  # end
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes' do
+      expect {subject.release_bike}.to raise_error('No bikes available')
+    end
   end
 end
