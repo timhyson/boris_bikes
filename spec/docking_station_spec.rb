9becef0f568588af6829be64bc8f46acdd52f339
Tim Hyson <tjhyson@gmail.com>
2015-09-03 12:25:42 +0100

    Completed Challenges 1-10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require "docking_station"
+
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
@@ -6,16 +7,3 @@ describe DockingStation do
   bike = subject.release_bike
   expect(bike).to be_working
 end
-
-
- #
- #  it "expects the bike to be working" do
- #
- #   :working?
- # end
-end
-
-
-# { is_expected(my_object).to be_a_kind_of Bike }
-
-# "release_bike returns a new instance of the Bike class."
