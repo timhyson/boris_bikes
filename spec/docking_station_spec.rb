347f511c1205802d94d3baee2cd604a52813a03b
Tim Hyson <tjhyson@gmail.com>
2015-09-03 14:23:29 +0100

    Post-Challenge 1

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,9 @@ describe DockingStation do
   it "releases working bikes" do
   bike = subject.release_bike
   expect(bike).to be_working
+  end
+  it { is_expected.to respond_to :dock }
+  it 'should accept Bike.new as a argument' do 
+  	expect{ subject.dock(Bike.new) }.to_not raise_error
+  end
 end
