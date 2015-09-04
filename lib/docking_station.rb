e58a42db4b22defc41bcfbb0757c8d9fa3403bde
Tim Hyson <tjhyson@gmail.com>
2015-09-04 10:40:12 +0100

    Post Challenge 2 dock error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,12 +2,25 @@ require_relative 'bike'
 
 class DockingStation
   def release_bike
-    raise 'No bikes available' unless @bike
+    fail 'No bikes available' unless @bike
     @bike
   end
 
   def dock(bike)
-  	raise 'Docking Station full' if @bike
+  	fail 'Docking station full' if @bike
     @bike = bike
   end
-end
+end	
+
+
+# class DockingStation
+#   def dock(bike)
+#     fail 'Docking station full' if @bike
+#     @bike = bike
+#   end
+
+#   def release_bike
+#     fail 'No bikes available' unless @bike
+#     @bike
+#   end
+# end
